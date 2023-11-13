10c2e038a14c486d2950d85f1561080cb09b5ccf
Ziad Chamrah <147912275+Chamrah@users.noreply.github.com>
2023-11-13 21:35:32 +0100

    Add files via upload

---
+++ bisextile.py
@@ -0,0 +1,6 @@
+# Program that check if this year a leap year
+a=int(input("Enter a year : "))
+if ((a % 400 == 0)) :
+    print("this year leap year")
+else:
+    print("this year is not a leap year")
